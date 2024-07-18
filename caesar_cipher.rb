a2a2d593f48927c789c0d828bd884c56d5a7191f
Adhil Khan <adhilkhan2023@gmail.com>
2024-07-18 13:37:00 +0530

    Create a method that store case of letters of text
    The case of the letters of the ciphered text need to be the same as the
    case of the letters in the original text(text that given by user),
    that's why case of the letters of text is stored.

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -3,3 +3,18 @@ text = gets.chomp
 
 puts "Enter a number!"
 key = gets.chomp.to_i
+
+case_of_letters_of_text = []
+
+def store_case_of_letters_of_text(text, case_of_letters_of_text)
+	letters = text.split("")
+	letters.each do |letter|
+		if letter == letter.upcase
+			case_of_letters_of_text.push("upcase")
+		else
+			case_of_letters_of_text.push("downcase")
+		end
+	end
+end
+
+store_case_of_letters_of_text(text, case_of_letters_of_text)
