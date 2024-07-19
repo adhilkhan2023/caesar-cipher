c3beac56931c7352f283bb00efc2de0d9e798e02
Adhil Khan <adhilkhan2023@gmail.com>
2024-07-19 13:52:26 +0530

    Create a method set_correct_case
    set_correct_case method will set the correct case for the letters in
    ciphered text

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -20,3 +20,15 @@ end
 store_case_of_letters_of_text(text, case_of_letters_of_text)
 
 text.downcase!
+
+def set_correct_case(ciphered_text, case_of_letters_of_text)
+	ciphered_text_letters_with_correct_case = []
+	ciphered_text.each_with_index do |letter, index|
+		if case_of_letters_of_text[index] == "upcase"
+			ciphered_text_letters_with_correct_case.push(letter.upcase)
+		else
+			ciphered_text_letters_with_correct_case.push(letter.downcase)
+		end
+	end
+	ciphered_text_letters_with_correct_case.join
+end
